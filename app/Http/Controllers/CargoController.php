1c0bf02510099dc068eb8ee38ec2f8d839cb12d0
jose <joseruizperez635@gamil.com>
2026-07-06 09:13:23 -0500

    Crud de cargos implementados

---
+++ app/Http/Controllers/CargoController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Request;
+
+class CargoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $page=request('page',1);
+      $response = Http::withToken(session('token'))->get(env('API_URL').'/cargos?page='.$page);
+        $respuesta = $response->json();
+        return view('Cargos.index', [
+            'datos'=>$respuesta['data'],
+            'paginacion'=>$respuesta
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return View('cargos.crear');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+                $response = Http::withToken(session('token'))->post(env('API_URL').'/cargos',[
+            'nombre_cargo'=>$request->nombre,
+            'descripcion'=>$request->descripcion,
+
+        ]);
+             return redirect()->route('cargos.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+      $response = Http::withToken(session('token'))->get(env('API_URL')."/detalle_cargos/{$id}");
+      
+      $datos=$response->json();
+      return View('cargos.detalle',compact('datos'));
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+    $response = Http::withToken(session('token'))->get(env('API_URL')."/cargos/{$id}");
+      
+      $datos=$response->json();
+      return View('cargos.edit',compact('datos'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+      $response =Http::withToken(session('token'))->delete(env('API_URL')."/cargos/{$id}");
+           return redirect()->route('cargos.index');
+      
+    }
+}
---
+++ resources/views/Cargos/edit.blade.php
@@ -0,0 +1,86 @@
+<x-layouts::app :title="__('Dashboard')">
+    <div class="min-h-screen bg-slate-100 flex items-center justify-center p-8">
+
+        <div class="w-full max-w-4xl bg-white rounded-3xl shadow-2xl overflow-hidden">
+
+            <!-- Encabezado -->
+            <div class="bg-gradient-to-r from-blue-600 to-indigo-700 px-8 py-6">
+                <h1 class="text-3xl font-bold text-white">
+                    Registrar Empleado
+                </h1>
+
+                <p class="text-blue-100 mt-2">
+                    Complete la información del empleado.
+                </p>
+            </div>
+
+            <!-- Contenido -->
+            <form action="{{ route('cargos.update', $datos['id']) }}" method="post">
+                @method('PUT')
+                <div class="p-8">
+                    @csrf
+
+                    <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+
+                        <!-- Nombre -->
+                        <div>
+                            <label class="block mb-2 font-semibold text-gray-700">
+                                Nombre
+                            </label>
+
+                            <input name="nombre" type="text" placeholder="Ingrese el nombre"
+                                value="{{ $datos['nombre_cargo'] }}"
+                                class="w-full rounded-xl border border-gray-300 px-4 py-3 focus:ring-2 focus:ring-blue-500 outline-none">
+                        </div>
+
+                        <!-- Apellido -->
+
+                        <!-- Salario -->
+                        <div>
+                            <label class="block mb-2 font-semibold text-gray-700">
+                                Salario
+                            </label>
+
+                            <input name="salario" type="number" placeholder="$0" value="{{ $datos['funciones'] }}"
+                                class="w-full rounded-xl border border-gray-300 px-4 py-3 focus:ring-2 focus:ring-blue-500 outline-none">
+                        </div>
+
+                        <!-- Cargo -->
+                        <div>
+                            <label class="block mb-2 font-semibold text-gray-700">
+                                Cargo
+                            </label>
+
+                            <select name="cargo" id="cargo"
+                                class="w-full rounded-xl border border-gray-300 px-4 py-3 bg-white focus:ring-2 focus:ring-blue-500 outline-none">
+
+                            </select>
+                        </div>
+
+                    </div>
+
+                    <!-- Funciones -->
+
+                    <!-- Botones -->
+                    <div class="flex justify-end gap-4 mt-10">
+
+                        <a href="{{ route('cargos.index') }}"
+                            class="px-6 py-3 rounded-xl bg-gray-200 hover:bg-gray-300 font-semibold transition">
+                            Cancelar
+                        </a>
+
+                        <button type="submit"
+                            class="px-8 py-3 rounded-xl bg-blue-600 hover:bg-blue-700 text-white font-semibold shadow-lg transition">
+                            actualizar cargo
+                        </button>
+
+                    </div>
+            </form>
+
+        </div>
+
+    </div>
+
+    </div>
+
+</x-layouts::app>
